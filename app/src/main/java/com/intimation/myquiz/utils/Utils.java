07250b835d7588d34a417a90187c16762e08ec04
sankjoseph <sankjoseph@gmail.com>
2016-09-30 10:27:55 +0530

    workingMYQuiz
    done

---
+++ app/src/main/java/com/intimation/myquiz/utils/Utils.java
@@ -0,0 +1,18 @@
+package com.intimation.myquiz.utils;
+
+/**
+ * Created by gorillalogic on 6/12/15.
+ */
+public class Utils {
+
+    public static final String URL_VIEW_QUESTION = "http://www.intimationsoftware.com/ws/questionbank/viewquestion.php?topicid=1&clientid=1&appid=1";
+    //public static final String URL_VIEW_QUESTION = " http://www.entekrishi.com/mobile/login/?username=kirankrishnan@axtecindia.com&password=kiran123&device=mydeviceId";
+
+    public static String getTotalTime(int no) {
+        int minutes = no * 1;
+        String mins = (minutes % 60) + " Minutes";
+        if (minutes > 59)
+            mins = String.format("%02d", (minutes / 60)) + " : " + (minutes % (60 * (minutes/60))) + " Minutes";
+        return mins;
+    }
+}
